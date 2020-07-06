15cc5229b9877bda2b7f8b1b28add2122ed05ac6
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-07-06 18:55:28 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+// data
+const products = [
+  { id: 1, description: "Quarz Luxe", price: 12, img: 'assets/img/quarz-luxe.JPG'},
+  { id: 2, description: 'Curren Business', price: 20, img: 'assets/img/curren-business.JPG'},
+  { id: 3, description: 'Curren Sport', price: 5, img: 'assets/img/curren-sport.JPG'},
+  { id: 4, description: 'Jaragar Racing', price: 8, img: 'assets/img/jaragar-racing.JPG'},
+  { id: 5, description: 'Liges Hommes', price: 3, img: 'assets/img/liges-hommes.JPG'},
+  { id: 6, description: 'Maserati Mechanical', price: 65, img: 'assets/img/maserati-mechanical.JPG'},
+  { id: 7, description: 'Montre Mecanique', price: 25, img: 'assets/img/montre-mecanique.JPG'},
+  { id: 8, description: 'Brand Designer', price: 28, img: 'assets/img/brand-designer.JPG'},
+  { id: 9, description: 'Relogio Masculino', price: 4, img: 'assets/img/relogio-masculino.JPG'},
+  { id: 10, description: 'Tissot Multifunction', price: 29, img: 'assets/img/tissot-multifunction.JPG'},
+  { id: 11, description: 'Hip Hop Gold', price: 87, img: 'assets/img/hiphop-gold.JPG'},
+  { id: 12, description: 'Mesh Genova', price: 6, img: 'assets/img/mesh-genova.JPG'},
+];
+
 // set little red dot above icons nav
 window.addEventListener("load", () => {
   let checkbox = document.getElementsByTagName('input'); 
