6b7d607999bdbd7359b3bf0ee45d6fbeaf8c86f7
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-07-08 12:52:20 +0200

    Update vue.js

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -14,56 +14,55 @@ const products = [
   { id: 12, description: 'Mesh Genova', price: 6, img: 'assets/img/mesh-genova.JPG'},
 ];
 
-// component 
 const Home = {
-  template: '#home', 
+  template: '#home',
   name: 'Home',
   data: () => {
     return {
-      products, 
+      products,
       searchKey: '',
       liked: [],
       cart: []
     }
   },
   computed: {
-    filteredList() {
+    filteredList(){
       return this.products.filter((product) => {
         return product.description.toLowerCase().includes(this.searchKey.toLowerCase());
-      });
+      })
     },
-    getLikeCookie() {
+    getLikeCookie(){
       let cookieValue = JSON.parse($cookies.get('like'));
-      cookieValue == null ? this.liked = [] : this.liked = cookieValue; 
+      cookieValue == null ? this.liked = [] : this.liked = cookieValue
     },
-    cartTotalAmount() {     
+    cartTotalAmount(){
       let total = 0;
-      for (let item in this.cart) {
-        total = total + (this.cart[item].quantity * this.cart[item].price);
+      for (let item in this.cart){
+        total = total + (this.cart[item].quantity * this.cart[item].price)
       }
       return total;
     },
     itemTotalAmount(){
       let itemTotal = 0;
-      for (let item in this.cart) {
-        itemTotal = itemTotal + (this.cart[item].quantity);
+      for (let item in this.cart){
+        itemTotal = itemTotal + (this.cart[item].quantity);          
       }
       return itemTotal;
     }
   },
   methods: {
-    setLikeCookie() {
+    setLikeCookie(){
       document.addEventListener('input', () => {
         setTimeout(() => {
-          $cookies.set('like', JSON.stringify(this.liked)); 
+          $cookies.set('like', JSON.stringify(this.liked));
         }, 300);
       })
     },
-    addToCart(product) {
+    addToCart(product){
       // check if already in array
-      for (let i = 0; i < this.cart.length; i++) {
+      for (let i = 0; i < this.cart.length; i++){
         if (this.cart[i].id === product.id) {
-          return this.cart[i].quantity++ 
+          return this.cart[i].quantity++
         }
       }
       this.cart.push({
@@ -74,32 +73,31 @@ const Home = {
         quantity: 1
       })
     },
-    cartPlusOne(product) {
+    cartPlusOne(product){
       product.quantity = product.quantity + 1;
     },
-    cartMinusOne(product, id) {
+    cartMinusOne(product, id){
       if (product.quantity == 1) {
         this.cartRemoveItem(id);
       } else {
-        product.quantity = product.quantity - 1;
+        product.quantity = product.quantity -1;
       }
     },
-    cartRemoveItem(id) {
-      this.$delete(this.cart, id)   
+    cartRemoveItem(id){
+      this.$delete(this.cart, id)
     }
   },
-  mounted: function(){
+  mounted: () => {
     this.getLikeCookie;
   }
-}
-
-const WishList = {
-  template: '<h1>Wish List</h1>',
-  name: 'WishList',
 }
 const UserSettings = {
   template: '<h1>User Settings</h1>',
   name: 'UserSettings'
+}
+const WishList = {
+  template: '<h1>Wish List</h1>',
+  name: 'WishList'
 }
 const ShoppingCart = {
   template: '<h1>Shopping Cart</h1>',
@@ -108,11 +106,11 @@ const ShoppingCart = {
 
 // router
 const router = new VueRouter({
-  routes: [   
-    { path: '/', component: Home, name:'Home' },
-    { path: '/user-settings', component: UserSettings, name:'UserSettings' },
-    { path: '/wish-list', component: WishList, name:'WishList' },
-    { path: '/shopping-cart', component: ShoppingCart, name:'ShoppingCart'}
+  routes: [
+    { path: '/', component: Home, name: 'Home' },
+    { path: '/user-settings', component: UserSettings, name : 'UserSettings' },
+    { path: '/wish-list', component: WishList, name: 'WishList' },
+    { path: '/shopping-cart', component: ShoppingCart, name: 'ShoppingCart' },
   ]
 })
 
